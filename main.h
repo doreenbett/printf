e50d08ccb4e40f4f6c04d18be954220471d1d474
doreenbett <doreenbett001@gmail.com>
2021-10-05 07:09:54 +0000

    Fixing codes

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <limits.h>
-#include <stdio.>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 #endif
--- newprintf.c
+++ newprintf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
 /**
 *_printf - recreates the printf function
