f219f4880aa716f5df146ea5c5a0d04ccf7bdffc
Kwamboka1 <makorimildred@gmail.com>
2021-10-06 10:20:11 +0300

    Update main.h

---
+++ main.h
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+<<<<<<< Updated upstream
+/**
+*struct convert - specifies a struct
+*@sym: test character
+*@f: the functions associated
+*
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+/*main functions*/
+int _printf(const char *format, ...);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int _write_char(char);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int reversed(va_list);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+/*extra functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsigned_number(unsigned int);
+=======
+
+/*
+*struct - specifier - specifies a struct
+*struct specifier - specifies a struct
+*@test: test character
+*@f: the functions associated
+*/
+
+typedef struct specifier
+{
+	char *test;
+	int (*f)(va_list args);
+}spec;
+
+
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_percent(va_list args);
+int (*find_func(char a))(va_list);
+
+
+>>>>>>> Stashed changes
+#endif
