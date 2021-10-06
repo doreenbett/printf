8405e2509632ef4aa03de6ec89178dc73fbc6616
doreenbett <doreenbett001@gmail.com>
2021-10-06 10:41:36 +0000

    Modified printf.c

--- newprintf.c
+++ newprintf.c
@@ -1,68 +1,43 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 /**
-*find_func - looks for the specifier
-*@P: character
-*Return: NULL
-*/
-int (*find_func(char P))(va_list args)
-{
-	int i = 0;
-
-	spec arr[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"%", print_percent},
-		{NULL, NULL}
-	};
-	while (arr[i].test)
-	{
-		if (arr[i].test[0] == P)
-			return (arr[i].f);
-		i++;
-	}
-	return (NULL);
-}
-
-/**
-*_printf - recreates the printf function
-*@format: string with format specifier
-*Return: number of characters printed
-*
-*/
-
+ * _printf - printf function
+ * @format: const char pointer
+ * Return: b_len
+ */
 int _printf(const char *format, ...)
 {
-	int count, i = 0;
-	int (*m)(va_list);
-	va_list args;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	va_start(args, format);
-	i = 0;
-	if (format[0] == '%' && format[1] == '\0')
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	while (format != NULL && format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			if (format[i + 1] == '%')
+			p++;
+			if (*p == '%')
 			{
-				count += putchar(format[i]);
-				i++;
+				count += _putchar('%');
+				continue;
 			}
-			else
-			{
-				m = find_func(format[i + 1]);
-				count += m(args);
-				i += 2;
-			}
-		}
-		count += putchar(format[i]);
-		i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	putchar('\n');
-	va_end(args);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
